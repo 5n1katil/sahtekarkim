6dd58d12dabc9be2da7648478ca5b2b3a9e19103
5n1katil <5n1katildedektiflikoyunu@gmail.com>
2025-07-28 22:18:17 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyBX_Tme2B-2g2Rtj53WBfgmZ5QsE0UN1Bw",
   authDomain: "detektif-c17bb.firebaseapp.com",
@@ -8,38 +7,5 @@ const firebaseConfig = {
   appId: "1:422256375848:web:873b0a6372c992accf9d1d",
   databaseURL: "https://detektif-c17bb-default-rtdb.firebaseio.com/"
 };
-
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
-// 1) Eski API için window.db (gameLogic.js ile uyumlu)
-window.db = db;
-
-// 2) Gelişmiş yardımcı fonksiyonlar
-window.firebaseUtils = {
-  createRoom: function (roomCode, hostName) {
-    const roomRef = db.ref(`rooms/${roomCode}`);
-    return roomRef.set({
-      players: [{ name: hostName }],
-      createdAt: Date.now()
-    });
-  },
-  addPlayer: function (roomCode, playerName) {
-    const playersRef = db.ref(`rooms/${roomCode}/players`);
-    return playersRef.once("value").then(snapshot => {
-      const players = snapshot.val() || [];
-      if (players.some(p => p.name === playerName)) {
-        return Promise.reject("Bu isim zaten ekli.");
-      }
-      players.push({ name: playerName });
-      return playersRef.set(players);
-    });
-  },
-  listenToRoomChanges: function (roomCode, callback) {
-    const playersRef = db.ref(`rooms/${roomCode}/players`);
-    playersRef.on("value", snapshot => {
-      const players = snapshot.val() || [];
-      callback(players);
-    });
-  }
-};
+window.db = firebase.database();
