dcec33d77bd60fb7beeb5af62a9e459f4d7b5fb3
5n1katil <5n1katildedektiflikoyunu@gmail.com>
2025-07-28 15:03:46 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// firebase.js
+
+// Firebase ayarlarını içerir ve dışa aktarır
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX_Tme2B-2g2Rtj53WBfgmZ5QsE0UN1Bw",
+  authDomain: "detektif-c17bb.firebaseapp.com",
+  projectId: "detektif-c17bb",
+  storageBucket: "detektif-c17bb.appspot.com",
+  messagingSenderId: "422256375848",
+  appId: "1:422256375848:web:873b0a6372c992accf9d1d",
+  databaseURL: "https://detektif-c17bb-default-rtdb.firebaseio.com/"
+};
+
+// Firebase'i başlat
+firebase.initializeApp(firebaseConfig);
+
+// Veritabanı referansını dışa aktar
+const db = firebase.database();
+
+// Diğer dosyalarda kullanmak üzere dışa aktar
+window.db = db;
