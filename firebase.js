17cf216949f94beb922d84250e72d5e34863d80b
5n1katil <5n1katildedektiflikoyunu@gmail.com>
2025-07-30 15:14:54 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,30 @@
+<!-- Firebase SDK (App ve Database) -->
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js"></script>
+
+<script>
+/***********************
+ * Firebase Config
+ ***********************/
+const firebaseConfig = {
+  apiKey: "AIzaSyBX_Tme2B-2g2Rtj53WBfgmZ5QsE0UN1Bw",
+  authDomain: "detektif-c17bb.firebaseapp.com",
+  databaseURL: "https://detektif-c17bb-default-rtdb.firebaseio.com",
+  projectId: "detektif-c17bb",
+  storageBucket: "detektif-c17bb.firebasestorage.app",
+  messagingSenderId: "422256375848",
+  appId: "1:422256375848:web:873b0a6372c992accf9d1d",
+};
+
+/***********************
+ * Firebase Başlat
+ ***********************/
+firebase.initializeApp(firebaseConfig);
+
+/***********************
+ * Realtime Database Referansı
+ ***********************/
+window.db = firebase.database();
+
+console.log("✅ Firebase bağlandı ve window.db tanımlandı.");
+</script>
