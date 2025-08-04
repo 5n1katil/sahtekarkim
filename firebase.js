73ac56d4a8777e128616c322cc3cba6bfbb731e7
5n1katil <5n1katildedektiflikoyunu@gmail.com>
2025-08-04 13:23:16 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   authDomain: "detektif-c17bb.firebaseapp.com",
   databaseURL: "https://detektif-c17bb-default-rtdb.firebaseio.com",
   projectId: "detektif-c17bb",
-  storageBucket: "detektif-c17bb.firebasestorage.app",
+  storageBucket: "detektif-c17bb.appspot.com",  // Düzeltilmiş!
   messagingSenderId: "422256375848",
   appId: "1:422256375848:web:873b0a6372c992accf9d1d"
 };
@@ -20,3 +20,29 @@ firebase.initializeApp(firebaseConfig);
  * Realtime Database Referansı
  ***********************/
 window.db = firebase.database();
+
+/***********************
+ * Anonim Kimlik Doğrulama
+ * - Kullanıcıyı otomatik olarak anonim giriş yaptırır
+ * - UID ile kullanıcıyı tanıyabiliriz
+ ***********************/
+if (firebase.auth) {
+  window.auth = firebase.auth();
+
+  // Anonim giriş
+  window.auth.signInAnonymously()
+    .then(() => {
+      console.log("Anonim giriş başarılı. UID:", window.auth.currentUser.uid);
+    })
+    .catch(err => console.error("Anonim giriş hatası:", err));
+
+  // Kullanıcı değişimini dinle
+  window.auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log("Aktif UID:", user.uid);
+      window.myUid = user.uid;
+    } else {
+      console.log("Çıkış yapıldı.");
+    }
+  });
+}
