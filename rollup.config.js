0da888b5edf48f37ede9105effc17f903b7059ad
5n1katil <5n1katildedektiflikoyunu@gmail.com>
2025-12-27 10:14:54 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,33 +2,20 @@ const babel = require("@rollup/plugin-babel").default;
 const commonjs = require("@rollup/plugin-commonjs").default;
 const resolve = require("@rollup/plugin-node-resolve").default;
 
-const createPlugins = () => [
-  resolve({ browser: true, preferBuiltins: false }),
-  commonjs(),
-  babel({
-    babelHelpers: "bundled",
-    presets: [["@babel/preset-env", { targets: "> 0.25%, not dead" }]],
-    exclude: "node_modules/**",
-  }),
-];
-
-module.exports = [
-  {
-    input: "main.js",
-    output: {
-      file: "main.nomodule.js",
-      format: "iife",
-      sourcemap: false,
-    },
-    plugins: createPlugins(),
+module.exports = {
+  input: "main.js",
+  output: {
+    file: "main.nomodule.js",
+    format: "iife",
+    sourcemap: false,
   },
-  {
-    input: "firebase.js",
-    output: {
-      file: "firebase.bundle.js",
-      format: "esm",
-      sourcemap: false,
-    },
-    plugins: createPlugins(),
-  },
-];
+  plugins: [
+    resolve({ browser: true }),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      presets: [["@babel/preset-env", { targets: "> 0.25%, not dead" }]],
+      exclude: "node_modules/**",
+    }),
+  ],
+};
